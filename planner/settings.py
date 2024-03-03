3ee40b8b6546e41b5940e26ded35ec9d5c30c9e5
Stephane Jean <9010976+distephano30@users.noreply.github.com>
2024-03-02 19:13:05 -0500

    Update settings.py

--- planner/settings.py
+++ planner/settings.py
@@ -15,9 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-from dotenv import load_dotenv
-env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
-load_dotenv(env_path)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
